079172b0d73a5392a21b4de9d0f31b277811eeb3
Peter <pkurjanowicz5@gmail.com>
2019-06-24 20:05:57 -0600

    Pete's commit

--- main.py
+++ main.py
@@ -14,23 +14,21 @@ terrible_movies = [
     "Starship Troopers"
 ]
 
-def get_current_watchlist():
-    # returns user's current watchlist--hard coded for now
-    return [ "Star Wars", "Minions", "Freaky Friday", "My Favorite Martian" ]
+watch_list = []
 
 
 @app.route("/crossoff", methods=['POST'])
 def crossoff_movie():
     crossed_off_movie = request.form['crossed-off-movie']
 
-    if crossed_off_movie not in get_current_watchlist():
+    if crossed_off_movie not in watch_list:
         # the user tried to cross off a movie that isn't in their list,
         # so we redirect back to the front page and tell them what went wrong
         error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
 
         # redirect to homepage, and include error as a query parameter in the URL
         return redirect("/?error=" + error)
-
+    watch_list.remove(crossed_off_movie)
     # if we didn't redirect by now, then all is well
     return render_template('crossoff.html', crossed_off_movie=crossed_off_movie)
 
@@ -49,28 +47,13 @@ def add_movie():
         error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
         return redirect("/?error=" + error)
 
-    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-    new_movie_escaped = cgi.escape(new_movie, quote=True)
-
-    # TODO:
-    # Create a template called add-confirmation.html inside your /templates directory
-    # Use that template to render the confirmation message instead of this temporary message below
-    return "Confirmation Message Under Construction..."
-
-# TODO:
-# Modify the edit.html file to display the watchlist in an unordered list with bullets in front of each movie.
-# Put the list between "Flicklist" and "Edit My Watchlist" under this heading: <h2>My Watchlist</h2>
-
-# TODO:
-# Change get_current_watchlist to return []. This simulates a user with an empty watchlist.
-# Modify edit.html to make sense in such a situation:
-#  First: Hide the <h2>My Watchlist</h2> and it's unordered list.
-#  Second: Hide the crossoff form, since there are no movies to cross off. 
-# Then you can change get_current_watchlist back to the list of hard-coded movies.
+    watch_list.append(new_movie)
+    return render_template("add-confirmation.html", movie=new_movie)
 
 @app.route("/")
 def index():
+
     encoded_error = request.args.get("error")
-    return render_template('edit.html', watchlist=get_current_watchlist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
+    return render_template('edit.html', watchlist=watch_list, error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 app.run()
